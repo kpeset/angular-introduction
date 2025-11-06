c77fbac298e8189f0ad761e19cfa5ea05d25bacd
Kevin PESET <kevin.peset@gmail.com>
2025-11-06 11:34:52 +0200

    Generate links in navbar

--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  links = [
+    {
+      label: 'Home',
+      path: '/',
+    },
+    {
+      label: 'Characters',
+      path: '/characters',
+    },
+    {
+      label: 'Game',
+      path: '/game',
+    },
+  ];
 }
