5f05ea5f9ae6d4eff9b065f34d67ca15b227691c
Kevin PESET <kevin.peset@gmail.com>
2025-11-06 12:25:16 +0200

    display characters with hardcoded data

---
+++ src/app/models/character.model.ts
@@ -0,0 +1,5 @@
+export interface Character {
+  id: number | string;
+  name: string;
+  image: string;
+}
--- src/app/pages/characters/characters.component.ts
+++ src/app/pages/characters/characters.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { CharacterCardComponent } from '../../components/character-card/character-card.component';
+import { Character } from '../../models/character.model';
 
 @Component({
   selector: 'app-characters',
   standalone: true,
-  imports: [],
+  imports: [CharacterCardComponent],
   templateUrl: './characters.component.html',
-  styleUrl: './characters.component.css'
+  styleUrl: './characters.component.css',
 })
 export class CharactersComponent {
-
+  characters: Character[] = [
+    {
+      id: 1,
+      name: 'Harry Potter',
+      image: 'https://ik.imagekit.io/hpapi/harry.jpg',
+    },
+    {
+      id: 2,
+      name: 'Hermione Grander',
+      image: 'https://ik.imagekit.io/hpapi/hermione.jpeg',
+    },
+    {
+      id: 3,
+      name: 'Ron Weasley',
+      image: 'https://ik.imagekit.io/hpapi/ron.jpg',
+    },
+  ];
 }
