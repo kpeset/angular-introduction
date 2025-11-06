9c7652735f9c7a84ce67006b16af5f393ceffc29
Kevin PESET <kevin.peset@gmail.com>
2025-11-06 10:42:20 +0200

    Routes and Components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'characters',
+    loadComponent: () =>
+      import('./pages/characters/characters.component').then(
+        (m) => m.CharactersComponent,
+      ),
+  },
+  {
+    path: 'game',
+    loadComponent: () =>
+      import('./pages/game/game.component').then((m) => m.GameComponent),
+  },
+];
